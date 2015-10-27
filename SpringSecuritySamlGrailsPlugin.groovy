95e24c19b74e402339b9d17041c2e801e599a8c1
baird <davidb@infront.net.au>
2015-10-27 18:10:19 +1100

    Version bump/deploy

--- SpringSecuritySamlGrailsPlugin.groovy
+++ SpringSecuritySamlGrailsPlugin.groovy
@@ -48,7 +48,7 @@ import org.apache.commons.httpclient.HttpClient
 
 class SpringSecuritySamlGrailsPlugin {
     // the plugin version
-    def version = "2.0.5"
+    def version = "2.0.6"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     // the other plugins this plugin depends on
